f7a80ce2c5f7b32a7dbd5ca71455bc344c02e418
LinKun <linkun199011@163.com>
2016-11-14 15:31:43 +0800

    init

---
+++ app/src/main/java/com/ustclin/voicecompanydemo/MainActivity.java
@@ -0,0 +1,35 @@
+package com.ustclin.voicecompanydemo;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+
+    private Context mContext;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mContext = this;
+        setContentView(R.layout.main);
+        Button btn1 = (Button) findViewById(R.id.btn1);
+
+        if (btn1 == null) {
+            return;
+        }
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(mContext, TtsDemo.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
